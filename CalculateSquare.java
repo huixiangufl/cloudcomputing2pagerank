e31aec659c98161880c8a919ac361f71b498e91b
Huixiang Chen <chenhuixiang5319@gmail.com>
2015-10-18 12:55:53 -0400

    pagerank assignment

--- CalculateSquare.java
+++ CalculateSquare.java
@@ -9,56 +9,48 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
 
-public class SortPageRank {
-  public static final int SORTMAX = 1000000000;
-  public static final double SORTMULT = 100000000.0f;
+public class CalculateSquare {
+  public static double globalSquare = 0.0;
 
   public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+    private Text Square = new Text();
     private Text RankValue = new Text();
-    private Text Page = new Text();
 
     public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
       String line = value.toString();
       String [] val = line.split("\t");
-      String page = val[0];
       String rankNeighbors = val[1];
+      String [] rankValues = rankNeighbors.split(" ");
 
-      double pagerank = 0.0;
-      int split = rankNeighbors.indexOf(" ");
-      if(split >= 0) {
-        pagerank = new Double(rankNeighbors.substring(0, split));
-      } else {//for nodes with no output degrees
-        pagerank = new Double(rankNeighbors);
-      }
+      double rankValue1 = new Double(rankValues[0]);
+      double rankValue2 = new Double(rankValues[1]);
+      double currentSquare = (rankValue1-rankValue2) * (rankValue1-rankValue2);
 
-      RankValue.set(String.valueOf( SORTMAX - (int) (pagerank * SORTMULT) ));
-      Page.set(page);
-      output.collect(RankValue, Page);
+      Square.set("square value:");
+      RankValue.set(String.valueOf(currentSquare));
+
+      output.collect(Square, RankValue);
     }
   }
 
-
-  public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {    	
-    private Text RankValue = new Text();
-    private int count = 0;
-
+  public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+    private Text GlobalSquareValue = new Text();
     public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-
-      while(values.hasNext() && count < 10) {
-        double pagerank = (SORTMAX - new Integer(key.toString())) / SORTMULT;
-        String pagerankStr = String.format("%12.10f", pagerank);
-        RankValue.set(pagerankStr);
-//        output.collect(RankValue, values.next());
-        output.collect(values.next(), RankValue);
-        count++;
+      double globalSquare = 0.0;
+      while(values.hasNext()) {
+        globalSquare += new Double(values.next().toString());
       }
-    }
 
+      GlobalSquareValue.set(String.valueOf(globalSquare));
+
+      output.collect(key, GlobalSquareValue);
+    }
   }
+
 
   public static void main(String[] args) throws Exception {
-    JobConf conf = new JobConf(SortPageRank.class);
-    conf.setJobName("sortpagerank");
+    JobConf conf = new JobConf(CalculateSquare.class);
+    conf.setJobName("calculatesquare");
 
     conf.setOutputKeyClass(Text.class);
     conf.setOutputValueClass(Text.class);
--- ConvertToOneFile.java
+++ ConvertToOneFile.java
@@ -9,13 +9,10 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
 
-public class SortPageRank {
-  public static final int SORTMAX = 1000000000;
-  public static final double SORTMULT = 100000000.0f;
-
+public class ConvertToOneFile {
   public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
-    private Text RankValue = new Text();
     private Text Page = new Text();
+    private Text RankValue = new Text();
 
     public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
       String line = value.toString();
@@ -26,39 +23,35 @@ public class SortPageRank {
       double pagerank = 0.0;
       int split = rankNeighbors.indexOf(" ");
       if(split >= 0) {
-        pagerank = new Double(rankNeighbors.substring(0, split));
-      } else {//for nodes with no output degrees
-        pagerank = new Double(rankNeighbors);
+        pagerank = new Double (rankNeighbors.substring(0, split));
+      } else {
+        pagerank = new Double (rankNeighbors);
       }
 
-      RankValue.set(String.valueOf( SORTMAX - (int) (pagerank * SORTMULT) ));
       Page.set(page);
-      output.collect(RankValue, Page);
+      RankValue.set(String.valueOf(pagerank));
+      if(line.length() > 3)
+       output.collect(Page, RankValue);
     }
   }
 
-
-  public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {    	
-    private Text RankValue = new Text();
-    private int count = 0;
-
+  public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+    private Text RankValues = new Text();
     public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-
-      while(values.hasNext() && count < 10) {
-        double pagerank = (SORTMAX - new Integer(key.toString())) / SORTMULT;
-        String pagerankStr = String.format("%12.10f", pagerank);
-        RankValue.set(pagerankStr);
-//        output.collect(RankValue, values.next());
-        output.collect(values.next(), RankValue);
-        count++;
+      String rankValues = "";
+      while(values.hasNext()) {
+        String rankValue = values.next().toString();
+        rankValues = rankValues + rankValue + " ";
       }
+      RankValues.set(rankValues);
+      output.collect(key, RankValues);
     }
-
   }
+
 
   public static void main(String[] args) throws Exception {
-    JobConf conf = new JobConf(SortPageRank.class);
-    conf.setJobName("sortpagerank");
+    JobConf conf = new JobConf(ConvertToOneFile.class);
+    conf.setJobName("converttoonefile");
 
     conf.setOutputKeyClass(Text.class);
     conf.setOutputValueClass(Text.class);
